13343255a5707a29156f740370c74899d63980cc
Michael Kleinhandler <mkleinha@uncc.edu>
2019-03-25 17:07:12 -0400

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
   get 'welcome/index'
   root 'welcome#index'
-  
-  get 'welcome/profile'
+
+  get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
