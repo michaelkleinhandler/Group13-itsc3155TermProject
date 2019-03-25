3c833b14fc40caab6fc865ffd8b2bbcbd5e7e0f2
Will French <ghoulbreak@gmail.com>
2019-03-25 21:04:52 +0000

    Added profile view, as well as fixed some formating and buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   }
   get 'welcome/index'
   root 'welcome#index'
+  
+  get 'welcome/profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
