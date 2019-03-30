0caec5dff691f3005889af44f16ae06e8315941a
Tia Valakuzhy <tvalakuz@uncc.edu>
2019-03-30 18:26:26 -0400

    added course controller file

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,48 @@
 class CourseController < ApplicationController
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to @course
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @course = Course.all
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      redirect_to @course
+
+    else
+      render 'edit'
+    end
+  end
+
+ def destroy
+   @course = Course.find(params[:id])
+   @course.destroy
+
+   redirect_to root_path
+ end
+
+  private
+  def course_params
+    params.require(:title).require(:teacher).require(:semester).require(:year).require(:subject).require(:course).require(:section)
+  end
+
 end
