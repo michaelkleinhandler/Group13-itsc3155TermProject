3e005e14dc732b695c2c2ea5e9ac272e910d2137
Michael Kleinhandler <michaelkleinhandler@gmail.com>
2019-04-24 02:58:40 +0000

    Removed added reference line in migration causing issues

--- db/migrate/20190405171306_add_uid_to_courses.rb
+++ db/migrate/20190405171306_add_uid_to_courses.rb
@@ -1,6 +1,5 @@
 class AddUidToCourses < ActiveRecord::Migration[5.2]
   def change
-    add_reference :courses, :User, foreign_key: true
-    reference
+    add_reference :courses, :User
   end
 end
