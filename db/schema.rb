84fb82779adba093e66e7c61738d8a3d016fee02
Michael Kleinhandler <mkleinha@uncc.edu>
2019-04-29 23:50:01 -0400

    Updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_055542) do
+ActiveRecord::Schema.define(version: 2019_04_30_031921) do
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
